31b2fed33af48ec7c30836158831f2d360cdcdfb
Renjie Sun <sunrenjie6@gmail.com>
2018-04-10 18:01:20 +0800

    startDerby.sh: run startNetworkServer from PATH
    The script is then freed from having to know where the derby distribution is
    located.

--- demo/geode-server-package/startDerby.sh
+++ demo/geode-server-package/startDerby.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-export DERBY_INSTALL=/huge/r/bin/db-derby-10.14.1.0-bin
-export CLASSPATH=$DERBY_INSTALL/lib/derbytools.jar:$DERBY_INSTALL/lib/derbynet.jar:.
-
 # -h 0.0.0.0 is for listening on all interfaces; see also:
 # http://db.apache.org/derby/docs/10.10/adminguide/cadminnetservsecurity.html
-java -jar $DERBY_INSTALL/lib/derbyrun.jar server start -h 0.0.0.0 > derby.log 2>&1&
+startNetworkServer -h 0.0.0.0 > derby.log 2>&1&
 echo $! > derby_pid.txt
 echo "Derby Started"
