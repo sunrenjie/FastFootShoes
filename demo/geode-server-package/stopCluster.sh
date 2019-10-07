34fbd2bb67147dfc8ac81fcd97de34f49ce9fd8d
Renjie Sun <sunrenjie6@gmail.com>
2019-10-07 09:40:15 +0800

    Clean up start-up scripts
    Motivation:
    The start-up scripts are copied literally from the original demo; significant changes have been made since then.
    
    Changes:
    1. Created an init.sh for set-up of the two-node setup; all data will be written to the prefix folder /data, which is included in .gitignore.
    2. Removed other obsolete files.
    3. Created a log4j2.xml that is to be used by all modules.
    
    Results:
    Now we could run the demo more easily.

--- demo/geode-server-package/stopCluster.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Randy May & Luke Shannon
-cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source setenv.sh
-gfsh -e "connect" -e "shutdown --include-locators=true"
-echo $CLOSE_MESSAGE
-read close_me
